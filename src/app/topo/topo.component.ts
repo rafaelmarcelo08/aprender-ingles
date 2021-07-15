9bb8c085a5f4930bb60fd8b563e08e564768a5a1
Rafael <rafael.2018.ads@gmail.com>
2021-07-15 16:00:35 -0300

    Instalando o Bootstrap no projeto

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,8 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-topo',
-    templateUrl: './topo.component.html',
-    styleUrls: ['']
+    templateUrl: './topo.component.html'
 })
 export class TopoComponent {
 
