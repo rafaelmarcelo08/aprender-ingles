37c934f9c43e0548b24aa65c8d38322288d8e406
Rafael <rafael.2018.ads@gmail.com>
2021-07-15 11:51:55 -0300

    Criado o componente Tentativas e Progresso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
