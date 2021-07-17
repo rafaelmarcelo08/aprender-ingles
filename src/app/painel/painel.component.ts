f8a23ab1b7716272c63e4f68970ee4617ca44fb2
Rafael <rafael.2018.ads@gmail.com>
2021-07-17 15:09:08 -0300

    Definindo resposta do usuario

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,12 +12,18 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
+  public resposta!: string;
 
   constructor() { 
     console.log(this.frases);
   }
 
   ngOnInit(): void {
+  }
+
+  atualizarResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    console.log(this.resposta);
   }
 
 }
